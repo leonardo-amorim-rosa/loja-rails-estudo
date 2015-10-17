64af9c2d281f58654ed54dabf3b65834120dcebe
Leonardo <leoamorimrosa@gmail.com>
2015-10-17 17:46:50 -0300

    add departamentos, icones, refactor

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,7 @@
 class ProdutosController < ApplicationController
 	
+	before_action :set_produto, only: [:edit, :update, :destroy]
+
 	def index
 		@produtos_por_nome = Produto.order(:nome).limit 5
 		@produtos_por_preco = Produto.order(:preco).limit 2
@@ -7,26 +9,54 @@ class ProdutosController < ApplicationController
 
 	def new 
 		@produto = Produto.new
+		renderiza(:new)
 	end
 
 	def busca
 		@produtos = Produto.where "nome like ?", "%#{params[:nome]}%"
 	end
 
+	def edit
+		renderiza(:edit)
+	end
+
+	def update
+		if @produto.update produto_params
+			flash[:notice] = "Produto alualizado com sucesso"
+			redirect_to root_url
+		else
+			renderiza(:edit)
+		end
+	end
+
 	def create
-		valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
-		@produto = Produto.new valores
+		@produto = Produto.new produto_params
 		if @produto.save
 			flash[:notice] = "Produto salvo com sucesso"
 			redirect_to root_url
 		else
-			render :new
+			renderiza(:new)
 		end			
 	end
 
 	def destroy
-		id = params[:id]
-		Produto.destroy id
+		@produto.destroy
 		redirect_to root_url
+	end
+
+	private
+
+	def renderiza (view)
+		@departamentos = Departamento.all
+		render view
+	end
+
+	def set_produto
+		id = params[:id]
+		@produto = Produto.find(id)
+	end
+
+	def produto_params
+		params.require(:produto).permit :nome, :descricao, :quantidade, :preco, :departamento_id
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :departamentos
 #  get "produtos" => "produtos#index"
 #  get "produtos/new" => "produtos#new"
 #  post "produtos" => "produtos#create"
 #  delete "produtos/:id/remover" => "produtos#destroy", as: :produto
-  resources :produtos, only: [:new, :create, :destroy]
+  resources :produtos, only: [:new, :create, :destroy, :edit, :update]
   get "produtos/busca" => "produtos#busca", as: :produto_busca	
   root "produtos#index"
 end
